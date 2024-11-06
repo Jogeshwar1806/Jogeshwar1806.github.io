e1e5d800b69021f257550a56e918fe4414825f02
Jogeshwar Mishra <jogeshwarmishra2003@gmail.com>
2024-11-06 15:18:18 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    document.querySelector('.nav-links').classList.toggle('active');
-}
-
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert("Your message has been sent!");
-});
